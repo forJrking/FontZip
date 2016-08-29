a87678b6b13bf2eee6bcce29b529ee03b5785d95
Jrking <forJrking@sina.com>
2016-08-29 20:34:53 +0800

    修复关闭后存在后台进程
    修复关闭后存在后台进程，添加EXE程序

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # 字体压缩神器
 >用于提取仅需要的美化文字，来压缩assets下的字体文件大小。
 
-![](https://img.shields.io/badge/support-OpenType-red.svg)
-![](https://img.shields.io/badge/support-TrueType-red.svg)
+![](https://img.shields.io/badge/support-OpenType-green.svg)
+![](https://img.shields.io/badge/support-TrueType-green.svg)
 ### Download
-- [***GUITool***](https://github.com/forJrking/FontZip/raw/master/FontZip.rar)
-- [***百度云下载***](http://pan.baidu.com/s/1sl09h97)
-
+- [**FontZip.jar**](https://github.com/forJrking/FontZip/raw/master/FontZip/FontZip1.0.rar)
+- [**百度云下载**](http://pan.baidu.com/s/1sl09h97)
+- [**FontZip.exe**](https://github.com/forJrking/FontZip/raw/master/FontZip/FontZip1.0.exe)
 ## GIF
 
-![GUI](gif.gif)
+![GUI](/img/gif.gif)
 
 ### GUITool使用说明:
 运行```run.bat```，选字体文件填入提取文字，点击OK，在字体同级目录下会生成 ```fontmin``` 文件，关闭GUI、命令窗口，稍等待5-10s后即可。
 
 经过测试，已经把项目***5MB***的艺术字体，按需求提取后，占用只有***20KB***，并且可正常使用。大大压缩了apk包的大小，还可以减小资源加载的占用。有此需要的同学赶紧用起来吧。支持otf、ttf、ttc，均测试提取成功。
 
-## ~~sfntly~~（请使用GUITool）
-[***~~sfnttool.jar~~下载***](https://github.com/forJrking/FontZip/raw/master/FontTool/sfnttool.jar)
+## ~~sfntly~~（请使用FontZip）
+[***~~sfnttool.jar~~下载***](https://github.com/forJrking/FontZip/raw/master/FontZip/sfnttool.jar)
 
 [***sfnttool.jar源码***](https://github.com/googlei18n/sfntly)
 
---
+++ src/gui/ToolGUI.java
@@ -0,0 +1,161 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import util.FontGenerateManager;
+
+public class ToolGUI extends JFrame implements MouseListener {
+
+
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[] args) {
+        new ToolGUI();
+    }
+
+    private String sourceFontFile;
+
+    private String generateFontFile;
+
+    private String endWith;
+
+    private JFrame frame;
+
+    private JTextArea viewArea;
+
+    private JLabel viewField, result;
+
+    private JButton button;
+
+    private JTextField fontName;
+
+    public ToolGUI() {
+        // 窗体
+        frame = new JFrame("字体压缩神器 By forjrking");
+        frame.setResizable(false);
+        // 文字区域
+        viewArea = new JTextArea(12, 35);// 可提取的字数
+
+        viewArea.setText("需要提取的文字");
+
+        viewArea.setFont(new Font("微软雅黑", Font.BOLD, 15));
+
+        fontName = new JTextField();
+        button = new JButton("OK");
+        viewField = new JLabel("选择源字体");
+        result = new JLabel("提醒:请勿使用fontmix做字体文件名");
+        result.setFont(new Font("微软雅黑", Font.BOLD , 16));
+        fontName.setColumns(25);
+
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 15, 15));
+        panel.add(viewField);
+        panel.add(fontName);
+        panel.add(button);
+
+        // 文字区域的拉动条
+        JScrollPane sp = new JScrollPane(viewArea);
+        sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        frame.add("South", result);
+        panel.add(sp);
+        frame.add("Center", panel);
+        frame.setSize(565, 365);
+        center(frame);
+        frame.setVisible(true);
+
+        button.addMouseListener((MouseListener) this);
+        fontName.addMouseListener((MouseListener) this);
+
+    }
+
+    void center(Window w) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension d = toolkit.getScreenSize();
+        int x = (d.width - w.getWidth()) / 2;
+        int y = (d.height - w.getHeight()) / 2;
+        w.setLocation(x, y);
+    }
+
+    public void mouseClicked(MouseEvent evt) {
+
+        if (evt.getSource() == button) {
+            if (isNotEmpty(viewArea.getText()) && isNotEmpty(fontName.getText())) {
+                // System.out.println("源文件" + sourceFontFile + "  生成文件名" +
+                // generateFontFile);
+                FontGenerateManager.generateFontByContent(viewArea.getText(), sourceFontFile, generateFontFile);
+                result.setText("结果：提取成功，请在字体同级目录查看");
+            } else {
+                result.setText("结果：请选择源字体并且添加提取的字");
+            }
+        } else if (evt.getSource() == fontName) {
+            fileChooser();
+        }
+    }
+
+    public boolean isNotEmpty(String src) {
+        return src != null && !src.equals("");
+    }
+
+    public void fileChooser() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(".ttf&.ttc", "TTC", "TTF");
+        // 设置文件类型
+        chooser.setFileFilter(filter);
+        // 打开选择器面板
+        int returnVal = chooser.showOpenDialog(new JPanel());
+        // 保存文件从这里入手，输出的是文件名
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            // chooser.getSelectedFile().getAbsolutePath());
+            fontName.setText(chooser.getSelectedFile().getName());
+            // 绝对路径
+            sourceFontFile = chooser.getSelectedFile().getAbsolutePath();
+            endWith = getExtName(chooser.getSelectedFile().getName(), '.');
+
+            String string = sourceFontFile.replace(chooser.getSelectedFile().getName(), "");
+            createGenerateFontFile(string);
+        }
+    }
+
+    private void createGenerateFontFile(String string) {
+        string = string.toLowerCase();
+        generateFontFile = string + "fontmix" + endWith;
+    }
+
+    private String getExtName(String s, char split) {
+        int i = s.indexOf(split);
+        int leg = s.length();
+        return (i > 0 ? (i + 1) == leg ? " " : s.substring(i, s.length()) : " ");
+    }
+
+    public void mousePressed(MouseEvent evt) {
+    }
+
+    public void mouseReleased(MouseEvent evt) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+}
